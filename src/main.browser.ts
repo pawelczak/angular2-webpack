a5a3e17727cc97f8baa6b63e938aea42cb04129d
Łukasz Pawełczak <l.pawelczak@icm.edu.pl>
2016-04-19 22:08:04 +0200

    Small change

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,5 +1,6 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 ///<reference path="../node_modules/angular2/ts/typings/jasmine/jasmine.d.ts"/>
+
 import 'angular2/bundles/angular2-polyfills.js';
 import 'rxjs/Rx';
 import {bootstrap} from 'angular2/platform/browser';
